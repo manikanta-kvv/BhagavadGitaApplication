cb45fb35b3526183697a20fdd326b37a09050b6b
manikanta-kvv <manikantakvv6@gmail.com>
2024-12-04 21:16:28 +0530

    working on emulator with no issues

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
+import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import TabNavigator from './src/navigation/TabNavigator';
 import { FavoritesProvider } from './src/context/FavoritesContext';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function App() {
   return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
     <FavoritesProvider>
       <SafeAreaProvider>
         <NavigationContainer>
@@ -12,5 +15,6 @@ export default function App() {
         </NavigationContainer>
       </SafeAreaProvider>
     </FavoritesProvider>
+    </GestureHandlerRootView>
   );
 } 
